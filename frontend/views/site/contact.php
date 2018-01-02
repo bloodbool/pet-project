7fd21f436027dc370c2909a16888f03b59da3876
bloodbool <pro100dimaa@gmail.com>
2018-01-02 19:18:27 +0200

    some features

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -13,11 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-    </p>
-
+    
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
